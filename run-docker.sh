fff6760a5ffd85965e36a395dbf9e172fba40239
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-06-10 18:08:16 +0200

    adapt comments

--- run-docker.sh
+++ run-docker.sh
@@ -2,7 +2,8 @@
 # the following mounts the local folder to the guest folder using a direct bind
 # volumes are to be preferred, but since we just actually want to share this data directly, this solution is fine
 
-# this sets the display address to the docker host. docker.for.mac.host.internal is a keyword used in docker networking.
+# -e DISPLAY=docker.for.mac.host.internal:0 -> this sets the display address to the docker host.
+# docker.for.mac.host.internal is a keyword used in docker networking.
 # X11 addresses are described here: https://askubuntu.com/questions/432255/what-is-the-display-environment-variable
 
 if docker ps | grep my-ubuntu > /dev/null; then
