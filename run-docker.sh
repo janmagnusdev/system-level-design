9f284da86a600dda40e10e9e9f5addae4d439af7
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-06-14 10:10:45 +0200

    add some todos to run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -6,6 +6,16 @@
 # docker.for.mac.host.internal is a keyword used in docker networking.
 # X11 addresses are described here: https://askubuntu.com/questions/432255/what-is-the-display-environment-variable
 
+# make sure to run
+# open -a XQuartz
+# xhost +
+# first
+
+# TODO: Create and Start instead of Run
+# TODO: Check if image is present, otherwise build, then create, start and attach
+# TODO: Check if container is present, but not running, then start and attach
+# TODO: Check if container is running, just attach
+
 if docker ps | grep my-ubuntu > /dev/null; then
   docker attach my-ubuntu
 else
@@ -15,11 +25,11 @@ else
     -e DISPLAY=docker.for.mac.host.internal:0 \
     -it \
     --network="bridge" \
-    --entrypoint bash \
     --name my-ubuntu \
     --cpus="4" \
     -v /Users/jan-magnus/personal-projects/system-level-design:/home/sld/system-level-design \
     -v ~/.Xauthority:/root/.Xauthority \
-    my-ubuntu
+    my-ubuntu \
+    /bin/bash
   docker attach my-ubuntu
 fi
