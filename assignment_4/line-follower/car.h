bce7735ea056e468920e32df06d28646371aed9a
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-06-17 09:44:19 +0200

    car.h

---
+++ assignment_4/line-follower/car.h
@@ -0,0 +1,30 @@
+#ifndef CAR_H_
+#define CAR_H_
+
+#include <simulation.h> // Irrlicht3D engine wrapper
+#include "data_types.h"
+
+#include <systemc>
+
+SC_MODULE( car )
+{
+public:
+    /* ----- sensor and controller ports ----- */
+    sc_core::sc_fifo_out<sensor_data> sensors;
+    sc_core::sc_fifo_in<sensor_data> control;
+
+    /* ----- alternate constructor declaration ---- */
+    SC_HAS_PROCESS( car );
+    car( sc_core::sc_module_name =
+         sc_core::sc_gen_unique_name("car") );
+
+private:
+    /* ----- process ---- */
+    void run_simulation();
+
+    simulation_engine e;
+};
+
+#endif
+
+/* vim: set ts=4 sw=4 tw=72 et :*/
