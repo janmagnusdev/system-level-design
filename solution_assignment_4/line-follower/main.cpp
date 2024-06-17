6b716047f08a535bf7ec5e11d243b476ddb5939e
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-06-17 10:24:47 +0200

    restore solution

--- solution_assignment_4/line-follower/main.cpp
+++ solution_assignment_4/line-follower/main.cpp
@@ -4,10 +4,9 @@
 
 int sc_main( int /* argc unused */, char* /* argv unused */[] )
 {
-  /* TODO: ----- testbench instantiation and simulation start ----- */
-  // TODO: instantiate testbench and start SystemC simulation
-
-
+  /* ----- testbench instantiation and simulation start ----- */
+  testbench t("testbench");
+  sc_core::sc_start();
   std::cout << "Simulation finished." << std::endl;
   return 0;
 }
