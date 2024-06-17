7d58c487e13fd28ae731a26378d70215cf7d9094
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-06-17 10:12:06 +0200

    comment main.cpp and initialise testbench.h a bit

--- assignment_4/line-follower/testbench.h
+++ assignment_4/line-follower/testbench.h
@@ -3,6 +3,7 @@
 
 #include "car.h"
 #include "car_controller.h"
+#include "data_types.h"
 
 
 #include <systemc>
@@ -10,16 +11,23 @@
 SC_MODULE(testbench)
 {
 public:
-    /* TODO: ----- submodules ----- */
-
+    /* ----- submodules ----- */
+    car m_car;
+    car_controller m_car_controller;
 
     /* TODO: ----- fifo channels ----- */
+    sc_core::sc_fifo<sensor_data> fifo_car_to_controller;
+    sc_core::sc_fifo<control_data> fifo_controller_to_car;
 
     /* TODO: ----- constructor ----- */
     SC_CTOR(testbench)
     /* TODO: ----- initialiser list ----- */
     {
         /* TODO: ----- port to channel binding ----- */
+        m_car.sensors(fifo_car_to_controller);
+        m_car_controller.sensors(fifo_car_to_controller);
+        m_car_controller.control(fifo_controller_to_car);
+        m_car_controller.control(fifo_controller_to_car);
     }
 
 };
--- solution_assignment_4/line-follower/main.cpp
+++ solution_assignment_4/line-follower/main.cpp
@@ -5,6 +5,7 @@
 int sc_main( int /* argc unused */, char* /* argv unused */[] )
 {
   /* TODO: ----- testbench instantiation and simulation start ----- */
+  // TODO: instantiate testbench and start SystemC simulation
 
 
   std::cout << "Simulation finished." << std::endl;
